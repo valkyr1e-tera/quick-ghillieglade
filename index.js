5833a7c60aace77705120ab11af50e734bad4769
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-26 10:13:18 +0900

    fix location

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ module.exports = function QuickGG(mod) {
 
   mod.hook('S_SPAWN_ME', 3, event => {
     if (mod.game.me.zone == 9713) {
-      event.loc = new Vec3({ x: 52229.47265625, y: 116954.140625, z: 4303.328125 })
+      event.loc = new Vec3(52229.47265625, 116954.140625, 4303.328125)
       event.w = 1.5
+
+      process.nextTick(() => {
+        mod.send('C_PLAYER_LOCATION', 5, {
+          loc: event.loc,
+          w: event.w,
+          lookDirection: 0,
+          dest: event.loc,
+          type: 7,
+          jumpDistance: 0,
+          inShuttle: 0,
+          time: 0
+        })
+      })
+
       return true
     }
   })
